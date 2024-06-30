0d1d98eb92f0e6b662b4c2d026bceacc4aafa217
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-06-30 22:44:59 +0300

    js güncellendi

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ let randomNum2;
 MyBtn.onclick = function () {
   randomNum1 = Math.floor(Math.random() * max) + min;
   randomNum2 = Math.floor(Math.random() * max) + min;
-  num1.textContent=randomNum1;
+  num1.textContent = randomNum1;
   num2.textContent = randomNum2;
 };

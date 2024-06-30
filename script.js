9e9b39f11d0e9e7666eeb9e1db5bfc22827af9ec
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-06-30 21:18:45 +0300

    randam number veren basit bir program oluşturuldu

---
+++ script.js
@@ -0,0 +1,15 @@
+const MyBtn = document.getElementById("MyBtn");
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const min = 1;
+const max = 6;
+
+let randomNum1;
+let randomNum2;
+
+MyBtn.onclick = function () {
+  randomNum1 = Math.floor(Math.random() * max) + min;
+  randomNum2 = Math.floor(Math.random() * max) + min;
+  num1.textContent=randomNum1;
+  num2.textContent = randomNum2;
+};
